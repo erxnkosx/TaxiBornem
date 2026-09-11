b7ee711337c1a392f4167098a1deff69decc4411
Taxi Bornem <info@taxibornem.be>
2026-09-11 20:30:38 +0200

    darkmode +prijs

--- src/data/site.ts
+++ src/data/site.ts
@@ -182,7 +182,7 @@ export const faqItems = [
 //  Schiphol             155    €423,50          €275          ~35%
 //  Köln/Bonn            225    €612,50          €349          ~43%
 export const destinations = [
-  { from: "Bornem", to: "Brussels Airport (Zaventem)", price: "€90", duration: "±40 min" },
+  { from: "Bornem", to: "Brussels Airport (Zaventem)", price: "€99", duration: "±40 min" },
   { from: "Bornem", to: "Antwerpen Airport (Deurne)", price: "€80", duration: "±30 min" },
   { from: "Bornem", to: "Charleroi Airport", price: "€199", duration: "±1u05" },
   { from: "Bornem", to: "Eindhoven Airport", price: "€249", duration: "±1u15" },
